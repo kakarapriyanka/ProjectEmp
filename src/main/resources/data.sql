89f8441458e4afb5f2f4917281c7c0fb46ffc153
Kakara Priyanka <priyanka.emrald@gmail.com>
2022-03-07 04:01:08 +0000

    First commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+insert into employee(id, name, last_name) 
+values(100,'anubha','biswas');
+insert into employee (id, name, last_name) 
+values(101,'sindhu' ,'komaravalli' );
+insert into employee(id, name, last_name ) 
+values(102,'sowmya', 'kammadanam');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
